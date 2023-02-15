e03a10a859cfcad751b2842c727c20c15a53cc8b
Muhammad Uzair Khan <u.khan@nagwaboutique.sa>
2023-02-15 21:23:53 +0500

    Configured Firebase

---
+++ Payment Reminder/src/firebase.js
@@ -0,0 +1,75 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+​​import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail ​​} from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpt7ZEvole66e39py_xHUjxOW_XT1o1wc",
+  authDomain: "payment-reminder-59f58.firebaseapp.com",
+  projectId: "payment-reminder-59f58",
+  storageBucket: "payment-reminder-59f58.appspot.com",
+  messagingSenderId: "604631456151",
+  appId: "1:604631456151:web:7ef61c431dcf5b1c1cccd2",
+  measurementId: "G-JF02Z7F63N",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+​​const auth = getAuth(app);
+
+
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const user = res.user;
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name,
+        authProvider: "local",
+        email,
+      });
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  const sendPasswordReset = async (email) => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert("Password reset link sent!");
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  const logout = () => {
+    signOut(auth);
+  };
+
+  export {
+    auth,
+    db,
+    logInWithEmailAndPassword,
+    registerWithEmailAndPassword,
+    sendPasswordReset,
+    logout,
+  };
