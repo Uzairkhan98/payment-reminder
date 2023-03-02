fce2beedf2d5b6c6621bf133c0d48b68465261eb
Muhammad Uzair Khan <khan.uzair@protonmail.com>
2023-03-03 01:51:00 +0500

    Update firebase.js

--- Payment Reminder/src/firebase.js
+++ Payment Reminder/src/firebase.js
@@ -11,13 +11,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpt7ZEvole66e39py_xHUjxOW_XT1o1wc",
-  authDomain: "payment-reminder-59f58.firebaseapp.com",
-  projectId: "payment-reminder-59f58",
-  storageBucket: "payment-reminder-59f58.appspot.com",
-  messagingSenderId: "604631456151",
-  appId: "1:604631456151:web:7ef61c431dcf5b1c1cccd2",
-  measurementId: "G-JF02Z7F63N",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
